3b1c863dac0a55f5487eae08cfcf4d70bd76d97c
ts-akasaka <tsunemasa.akasaka@gmail.com>
2022-01-07 22:59:51 +0900

    fix: createShuffledBoard may return incompletable.

--- src/lib/sliding.test.ts
+++ src/lib/sliding.test.ts
@@ -1,10 +1,17 @@
-import { isBoardComplete, findNextVacantPosOnBoard, findVacantPosOnBoard, swapNumberOnBoard, createShuffledBoard } from "./sliding";
+import { isBoardComplete, findNextVacantPosOnBoard, findVacantPosOnBoard, swapNumberOnBoard, createShuffledBoard, Board, getNextPositions } from "./sliding";
 
 test('isBoardCompleteは、完成した盤面に真を返す', () => {
   expect(isBoardComplete([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null])).toBe(true);
   expect(isBoardComplete([2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null])).toBe(false);
   expect(isBoardComplete([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, null, 15])).toBe(false);
   expect(isBoardComplete([null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])).toBe(false);
+});
+
+test('getNextPositionsは、隣の位置を配列で返す', () => {
+  expect(getNextPositions(0).sort()).toEqual([1, 4]);
+  expect(getNextPositions(1).sort()).toEqual([0, 2, 5]);
+  expect(getNextPositions(5).sort()).toEqual([1, 4, 6, 9]);
+  expect(getNextPositions(15).sort()).toEqual([11, 14]);
 });
 
 test('findNextVacantPosOnBoardは、指定された位置の隣のマスに空きマスがあればその位置を返す', () => {
@@ -32,4 +39,39 @@ test('swapNumberOnBoardは、盤面の指定位置の数マス（または空き
     .toEqual([1, 2, 3, 4, 9, 6, 7, 8, 5, 10, 11, 12, 13, 14, 15, null]);
   expect(swapNumberOnBoard([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, null, 13, 14, 15, 12], 15, 11))
     .toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null]);
+});
+
+test('createShuffledBoardは、シャッフルされた盤面を返す。', () => {
+  const brd = createShuffledBoard();
+  expect(brd.length === 16).toEqual(true);
+  expect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null].every(v => brd.includes(v))).toEqual(true);
+});
+
+test('createShuffledBoardでシャッフルされた盤面は、指定された手数で解くことができる。', () => {
+  // 指定手数でその盤面が解けるかをチェックする再帰関数。
+  const solver = (brd: Board, max: number, step = 0): boolean => {
+    if (step > max) {
+      return false;
+    } else if (isBoardComplete(brd)) {
+      return true;
+    } else {
+      const vpos = findVacantPosOnBoard(brd) ?? -100;
+      return getNextPositions(vpos).some(pos => solver(
+        swapNumberOnBoard(brd, vpos, pos),
+        max,
+        step + 1
+      ) === true);
+    }
+  };
+  // solver関数自体のテスト
+  expect(solver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null], 0)).toBeTruthy();
+  expect(solver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, null, 15], 1)).toBeTruthy();
+  expect(solver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, null, 15], 0)).toBeFalsy();
+  expect(solver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, null, 13, 14, 15], 3)).toBeTruthy();
+  expect(solver([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, null, 13, 14, 15], 2)).toBeFalsy();
+  // createShuffledBoardのテスト。
+  // シャッフルでの交換回数10回、最大手数10回で100回行い、全て解ける盤面かを確認する。
+  for (let i = 0; i < 100; i++) {
+    expect(solver(createShuffledBoard(10), 10)).toBeTruthy();
+  }
 });
--- src/lib/sliding.ts
+++ src/lib/sliding.ts
@@ -3,7 +3,7 @@ import { DeepReadonly } from 'utility-types';
 /**
  * 4 x 4 の盤面と数マスと空きマス
  */
- export type Board = (number | null)[];
+export type Board = (number | null)[];
 
 /**
  * 盤面上、1～15の数マスが揃った状態ならば真を返す。
@@ -21,25 +21,34 @@ export const isBoardComplete = (brd: DeepReadonly<Board>): boolean => {
 };
 
 /**
- * 指定された位置の隣のマスに空きマスがあればその位置を返す。
- * @param brd 盤面
- * @param pos 位置
- * @returns 空きマスがあればその位置、そうでなければnull
+ * 盤面上の位置を指定すると、その位置に隣り合う位置を配列で返す
+ * @param pos 位置（0～15）
+ * @returns 位置の配列（
  */
-export const findNextVacantPosOnBoard = (brd: DeepReadonly<Board>, pos: number): number | null => {
+export const getNextPositions = (pos: number) => {
   return [
     pos - 4 >= 0 ? pos - 4 : null,
     pos + 4 <= 15 ? pos + 4 : null,
     pos % 4 - 1 >= 0 ? pos - 1 : null,
     pos % 4 + 1 <= 3 ? pos + 1 : null,
-  ].find(
+  ].filter((v): v is number => typeof v === "number");
+};
+
+/**
+ * 指定された位置の隣のマスに空きマスがあればその位置を返す。
+ * @param brd 盤面
+ * @param pos 位置（0～15）
+ * @returns 空きマスがあればその位置、そうでなければnull
+ */
+export const findNextVacantPosOnBoard = (brd: DeepReadonly<Board>, pos: number): number | null => {
+  return getNextPositions(pos).find(
     p => p !== null && brd[p] === null
   ) ?? null;
 };
 
 /**
  * 盤面の中の空きマスの位置を返す。
- * @param brd 
+ * @param brd 盤面
  * @returns 空きマスが見つかればその位置、そうでなければnull
  */
 export const findVacantPosOnBoard = (brd: DeepReadonly<Board>): number | null => {
@@ -48,36 +57,32 @@ export const findVacantPosOnBoard = (brd: DeepReadonly<Board>): number | null =>
 
 /**
  * 盤面の指定位置の数マス（または空きマス）を交換する。
- * @param brd 
- * @param pos1 
- * @param pos2 
- * @returns 
+ * @param brd 盤面
+ * @param pos1 交換する盤面上の位置１（0～15）
+ * @param pos2 交換する盤面上の位置２（0～15）
+ * @returns 交換した結果の盤面（新規生成した配列）。
  */
 export const swapNumberOnBoard = (brd: DeepReadonly<Board>, pos1: number, pos2: number) => {
   const nbrd = [...brd];
   if (pos1 in brd && pos2 in brd && pos1 !== pos2) {
-    // const temp = nbrd[pos1];
-    // nbrd[pos1] = nbrd[pos2];
-    // nbrd[pos2] = temp;
     [nbrd[pos1], nbrd[pos2]] = [nbrd[pos2], nbrd[pos1]];
   }
   return nbrd;
 }
 
-const ShuffleCount = 100;
 const initialBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null];
 /**
- * シャッフルされた盤面を返す。
- * @returns シャッフルされた盤面
+ * シャッフル（空きマスと数字マスを指定回数、ランダムに交換）された盤面を返す。
+ * @param shuffleCount 空きマスと数字マスを交換する回数。デフォルトは100回。
+ * @returns 新規生成され、シャッフルされた盤面
  * Note: 単純なランダムソートでは、完成不可能な配置が発生する。
  */
-export const createShuffledBoard = (): Board => {
+export const createShuffledBoard = (shuffleCount = 100): Board => {
   let brd = [...initialBoard];
-  for (let i = 0; i < ShuffleCount; i++) {
+  for (let i = 0; i < shuffleCount; i++) {
     const vpos = findVacantPosOnBoard(brd) ?? -1;
-    const npos = [vpos - 4, vpos + 1, vpos + 4, vpos - 1]
-      .filter(p => p in brd)
-      .sort(() => Math.random() >= 0.5 ? -1 : 1)[0] ?? -1;
+    const nexts = getNextPositions(vpos);
+    const npos = nexts[Math.floor(Math.random() * nexts.length)];
     brd = swapNumberOnBoard(brd, vpos, npos);
   }
   return brd;
